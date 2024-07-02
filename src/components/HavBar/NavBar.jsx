0675212ecd9c795121cdd1754a860dac7a7fd4ed
ernis2005 <nhtfxnhtfx.9@gmail.com>
2024-07-02 07:43:50 +0600

    fix

--- src/components/HavBar/NavBar.jsx
+++ src/components/HavBar/NavBar.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import s from './page.module.scss';
-import { Logo } from '../../assets/svg/logo';
-import { HomeBlcak, PriceList, Qutfits } from '../../assets/svg/navList';
-import { Menu, MenuItem, Sidebar } from 'react-pro-sidebar';
-import { NavLink, useLocation } from 'react-router-dom';
+import React from "react";
+import s from "./page.module.scss";
+import { Logo } from "../../assets/svg/logo";
+import { HomeBlcak, PriceList, Qutfits } from "../../assets/svg/navList";
+import { Menu, MenuItem, Sidebar } from "react-pro-sidebar";
+import { NavLink, useLocation } from "react-router-dom";
 
 const NavBar = ({ isModal, setIsModal }) => {
   const location = useLocation();
@@ -13,27 +13,29 @@ const NavBar = ({ isModal, setIsModal }) => {
   return (
     <Sidebar collapsed={isModal} className={s.navbar}>
       <Logo />
-      <Menu menuItemStyles={{
-        button: {
-          [`&.active`]: {
-            backgroundColor: '#E8F2FC',
-            color: '#00B0F0',
-            position: "relative",
+      <Menu
+        menuItemStyles={{
+          button: {
+            [`&.active`]: {
+              backgroundColor: "#E8F2FC",
+              color: "#00B0F0",
+              position: "relative",
 
-            "&::before": {
-              content: '""',
-              position: "absolute",
-              top: 0,
-              left: 0,
-              height: "100%",
-              width: "6px",
-              backgroundColor: "#017DC3",
-              borderTopRightRadius: "9999px",
-              borderBottomRightRadius: "9999px"
-            }
-          }
-        }
-      }}>
+              "&::before": {
+                content: '""',
+                position: "absolute",
+                top: 0,
+                left: 0,
+                height: "100%",
+                width: "6px",
+                backgroundColor: "#017DC3",
+                borderTopRightRadius: "9999px",
+                borderBottomRightRadius: "9999px",
+              },
+            },
+          },
+        }}
+      >
         <MenuItem
           component={<NavLink to="/" />}
           icon={<HomeBlcak stroke={isActive("/") ? "#00B0F0" : "black"} />}
@@ -42,7 +44,16 @@ const NavBar = ({ isModal, setIsModal }) => {
         </MenuItem>
         <MenuItem
           component={<NavLink to="/price-list" />}
-          icon={<PriceList stroke={isActive("/price-list") || isActive("/price-list/create-a-price-list") ? "#00B0F0" : "black"} />}
+          icon={
+            <PriceList
+              stroke={
+                isActive("/price-list") ||
+                isActive("/price-list/create-a-price-list")
+                  ? "#00B0F0"
+                  : "black"
+              }
+            />
+          }
         >
           Прайс-лист
         </MenuItem>
@@ -50,7 +61,8 @@ const NavBar = ({ isModal, setIsModal }) => {
         <MenuItem
           component={<NavLink to="/qutfits" />}
           icon={<Qutfits stroke={isActive("/qutfits") ? "#00B0F0" : "black"} />}
-        >Наряды
+        >
+          Наряды
         </MenuItem>
       </Menu>
     </Sidebar>
--- src/page/Outfits/create-an-outfit/create-an-outfit.jsx
+++ src/page/Outfits/create-an-outfit/create-an-outfit.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./page.module.scss";
 import DivBg from "../../../components/Cards/DivBg/DivBg";
+import {
+  addDays,
+  isWeekend,
+  isSameMonth,
+  setMonth,
+  startOfMonth,
+} from "date-fns";
 import { CiDocumentation } from "../../../assets/svg/сreteAnOutfit";
+import DatePicker from "react-datepicker";
 const CreteAnOutfit = () => {
+  const [selectedDate, setSelectedDate] = useState(startOfMonth(new Date()));
+
+  const filterWeekends = (date) => {
+    return !isWeekend(date) && isSameMonth(date, setMonth(new Date(), 9)); // 9 - October
+  };
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
   return (
     <div className={s.page}>
       <DivBg>
@@ -10,7 +27,23 @@ const CreteAnOutfit = () => {
           <h3>Наряд № 2</h3>
           <CiDocumentation />
         </div>
-        <div className={s.from}></div>
+        <div className={s.from}>
+          <div className={s.from1}>
+            <label name="" id="">
+              <p>ФИО пациента*</p>
+              <input type="text" placeholder="Введите ФИО пациента" />
+            </label>
+            <label name="" id="">
+              <p>Дата рождения* </p>
+              <DatePicker
+                selected={selectedDate}
+                onChange={handleDateChange}
+                filterDate={filterWeekends}
+                inline // Отображает календарь в развернутом виде
+              />
+            </label>
+          </div>
+        </div>
       </DivBg>
     </div>
   );
